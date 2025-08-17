a33b07e1d5fa4e18e9633e7867ae5261fc094300
amirali139177 <amiralikahrizi91@gmail.com>
2025-08-17 20:06:02 +0330

    s

---
+++ PYT10.py
@@ -0,0 +1,11 @@
+# import random
+
+# def pizza():
+#     piza= ["peperoni" , "garlick stake" , "meet and mashrooms"]
+#     A=random.choice(piza)
+#     print (A,"pizza")
+# pizza()
+def taklif(num1, num2):
+    print(int(num1 + num2))
+
+taklif(5, 8)
