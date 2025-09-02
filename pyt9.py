b3d090feffba685e1a084ee120ec5e8cfb8b5134
amirali139177 <amiralikahrizi91@gmail.com>
2025-09-02 19:28:21 +0330

    a

---
+++ pyt9.py
@@ -0,0 +1,14 @@
+# def count_letters():
+#     word = input( "ADAD BEGO" )
+#     length = len(word)
+#     print(word, length)
+
+# count_letters()
+
+
+def reverse_word(word):
+    return word[::-1]
+user_input = input("kalame bego")
+reversed_text = reverse_word(user_input)
+print(user_input, reversed_text)
+
